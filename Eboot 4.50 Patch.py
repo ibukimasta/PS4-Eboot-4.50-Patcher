813b58df40cf98c52ceb27a7bc0efecce7333349
ibukimasta <ibukimasta@gmail.com>
2018-09-11 21:57:32 -0400

    Add files via upload

--- Eboot 4.50 Patch.py
+++ Eboot 4.50 Patch.py
@@ -7,7 +7,7 @@ with open("eboot.bin", "rb") as input_file:
 		print("Found Reversed 50008001 SDK")
 	content = content.replace(b"\x01\x80\x00\x05", b"\x01\x80\x50\x04")
 	if b"\x01\x80\x50\x04" in content:
-		print("Patched With 04508001 SDK")
+		print("SDK is Now 04508001")
 	if b"\x01\x80\x00\x05" not in content:
 		print("Already Patched")
 	
@@ -21,7 +21,7 @@ with open("eboot.bin", "rb") as input_file:
 		print("Found 50008001 SDK")
 	content = content.replace(b"\x05\x00\x80\x01", b"\x04\x50\x80\x01")
 	if b"\x04\x50\x80\x01" in content:
-		print("Patched With 04508001 SDK")
+		print("SDK is Now 04508001")
 	if b"\x01\x80\x00\x05" not in content:
 		print("Already Patched")
 
